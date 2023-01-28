eed3f9f7b82308400b008bfaa9fa4accf856bed2
puxinyu <454100270@qq.com>
2023-01-28 14:36:40 +0800

    gx

--- nuxt3-app/app.vue
+++ nuxt3-app/app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h3>hellow world</h3>
   </div>
 </template>
+
+<script setup>
+// 判断代码运行环境
+if (process.server) {
+  //运行在服务端
+  console.log("运行在 server");
+  const runTimeConfig = useRuntimeConfig();
+  console.log(runTimeConfig.appKey);
+}
+// 运行在客户端
+if (process.client) {
+  console.log("运行在 client");
+  const runTimeConfig = useRuntimeConfig();
+  console.log(runTimeConfig.public.baseURL);
+}
+</script>
--- nuxt3-app/nuxt.config.ts
+++ nuxt3-app/nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  runtimeConfig: {
+    // 运行时配置
+    appKey: "abbbv",
+    public: {
+      baseURL: "123213", // server || client 里面都可以获取
+    },
+  },
+});
